6d13ee56a7d5e8cd2daba9a6d5dc917a23b9712b
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-07-13 21:59:20 +0530

    commit for the signup

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
--- src/utils/LoginFrontendApi/LoginFrontendApi.js
+++ src/utils/LoginFrontendApi/LoginFrontendApi.js
@@ -1,7 +1,7 @@
 // here we are defining the host of the main api
-// export const host = 'http://localhost:8000';
+export const host = 'http://localhost:8000';
 
-export const host = 'https://project-x-backend-psc5.onrender.com';
+// export const host = 'https://project-x-backend-psc5.onrender.com';
 
 // api routes for the registration of a user bro
 
